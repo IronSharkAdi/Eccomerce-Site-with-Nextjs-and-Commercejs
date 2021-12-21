e79f1a04ab3b75b19dc65e47a29f116965785b34
IronSharkAdi <ironsharkadi2020@gmail.com>
2021-12-21 17:59:29 +0600

    checkout fix

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -7,12 +7,14 @@ import commerce from "../lib/Commercejs/commerce";
 import Loader from "react-loader-spinner";
 import Navbar from "../components/navbar/Navbar";
 import CartItem from "../components/Cart/CartItem";
+import NextLink from 'next/link'
+
 
 const cart: NextPage = () => {
-    type CART = any
+    type CART = any;
     const [cart, setCart] = useState<CART>();
     const [available, setAvailable] = useState(false);
-    const [empty, setEmpty] = useState(false)
+    const [empty, setEmpty] = useState(false);
     const classes = useStyles();
 
     const router = useRouter();
@@ -20,67 +22,86 @@ const cart: NextPage = () => {
     const fetchCart = async () => {
         const itemsCart = await commerce.cart.retrieve();
         return itemsCart;
-    }
+    };
 
-    const handleUpdateCartQty = async(id , quantity) =>{
-        const response = await commerce.cart.update(id, {quantity})
-        setCart(response.cart)
-        if(response.cart.subtotal.raw === 0){
-            setAvailable(false)
+    const handleUpdateCartQty = async (id, quantity) => {
+        const response = await commerce.cart.update(id, { quantity });
+        setCart(response.cart);
+        if (response.cart.subtotal.raw === 0) {
+            setAvailable(false);
         }
-    }
-    const handleRemoveFromCart = async(id)=>{
-        const response = await commerce.cart.remove(id)
-        setCart(response.cart)
-        console.log(response.cart)
-        if(response.cart.subtotal.raw === 0){
-            setAvailable(false)
+    };
+    const handleRemoveFromCart = async (id) => {
+        const response = await commerce.cart.remove(id);
+        setCart(response.cart);
+        console.log(response.cart);
+        if (response.cart.subtotal.raw === 0) {
+            setAvailable(false);
         }
-    }
-    const handleEmptyCart = async() =>{
-        const response = commerce.cart.empty()
-        setAvailable(false)
-        setCart(response.cart)
-    }
+    };
+    const handleEmptyCart = async () => {
+        const response = commerce.cart.empty();
+        setAvailable(false);
+        setCart(response.cart);
+    };
 
     useEffect(() => {
         if (!router.isReady) return;
-        const cartSetting = async () =>{
+        const cartSetting = async () => {
             const itemsCart = await fetchCart();
             await setCart(itemsCart);
             console.log(cart);
             console.log(itemsCart);
-            setEmpty(true)
+            setEmpty(true);
             if (itemsCart.total_items > 0) {
                 setAvailable(true);
             }
-        }
-        cartSetting()
-
-    }, [router.isReady])
+        };
+        cartSetting();
+    }, [router.isReady]);
 
     const FilledCart = () => (
         <>
             {/* <Typography variant={"subtitle1"} >You have some items in your shopping cart</Typography> */}
             <Grid container spacing={4}>
-                {cart.line_items.map(item =>(
+                {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem handleRemoveFromCart={handleRemoveFromCart} handleUpdateCartQty={handleUpdateCartQty} item={item}/>
-                    </Grid>  
+                        <CartItem
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleUpdateCartQty={handleUpdateCartQty}
+                            item={item}
+                        />
+                    </Grid>
                 ))}
-
             </Grid>
             <div className={classes.cardDetails}>
                 <Typography variant="h4">
-                       Subtotal: {cart.subtotal.formatted_with_symbol} 
+                    Subtotal: {cart.subtotal.formatted_with_symbol}
                 </Typography>
                 <div>
-                    <Button onClick={handleEmptyCart} className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary"  >
+                    <Button
+                        onClick={handleEmptyCart}
+                        className={classes.emptyButton}
+                        size="large"
+                        type="button"
+                        variant="contained"
+                        color="secondary"
+                    >
                         Empty Cart
                     </Button>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="primary" >
-                        Checkout
-                    </Button>
+                    <NextLink href="/checkout">
+                        <a>
+                            <Button
+                                className={classes.emptyButton}
+                                size="large"
+                                type="button"
+                                variant="contained"
+                                color="primary"
+                            >
+                                Checkout
+                            </Button>
+                        </a>
+                    </NextLink>
                 </div>
             </div>
         </>
@@ -95,20 +116,25 @@ const cart: NextPage = () => {
     );
     const Cart = () => <>{available ? <FilledCart /> : <EmptyCart />}</>;
 
-
-
     return (
         <>
-        <Navbar/>
-        <Container>
-            <div className={classes.toolbar} />
-            <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
-            {empty ? (
-                <Cart />
-            ) : (
-                <Loader type="Oval" color="#00BFFF" height={50} width={50} />
-            )}
-        </Container>
+            <Navbar />
+            <Container>
+                <div className={classes.toolbar} />
+                <Typography className={classes.title} variant="h3" gutterBottom>
+                    Your Shopping Cart
+                </Typography>
+                {empty ? (
+                    <Cart />
+                ) : (
+                    <Loader
+                        type="Oval"
+                        color="#00BFFF"
+                        height={50}
+                        width={50}
+                    />
+                )}
+            </Container>
         </>
     );
 };
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,12 +1,51 @@
+import { Paper, Step, StepLabel, Stepper, Typography } from "@mui/material"
 import { NextPage } from "next"
+import { useState } from "react"
+import useStyles from "../styles/pagesStyle/checkout"
 
 
 const checkout:NextPage =() => {
+    const classes = useStyles()
+    const [activeSteps, setactiveSteps] = useState(0)
+    const steps = ["Shipping Address", "Payment details"]
+    const Form = () =>(
+        <>
+        {activeSteps === 0 ? <AddressForm/> : <PaymentForm/>}
+        </>
+    )
+    const PaymentForm = () =>(
+        <>
 
+        </>
+    )
+    const AddressForm = () => (
+        <>
+        
+        </>
+    )
+    const Confirmation = () =>(
+        <>
+        
+        </>
+    )
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className={classes.toolbar} />
+            <main className={classes.layout}>
+                <Paper className={classes.paper}>
+                        <Typography variant={"h4"} align="center">Checkout</Typography>
+                        <Stepper activeStep={0} className={classes.stepper}>
+                            {steps.map(step =>(
+                                <Step key={step}>
+                                    <StepLabel>{step}</StepLabel>
+                                </Step>
+                            ))}
+                        </Stepper>
+                        {activeSteps === steps.length ? <Confirmation/> : <Form/>}
+                </Paper>
+
+            </main>
+        </>
     )
 }
 
